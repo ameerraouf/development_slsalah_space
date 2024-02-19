f74076b1ef467ff849d576b57c63d333f3421c4a
MostafaSaber17 <mostaphasaper17@gmail.com>
2024-02-19 15:39:28 +0200

    [DEV]: Search & Favorite

--- resources/views/layouts/primary.blade.php
+++ resources/views/layouts/primary.blade.php
@@ -515,6 +515,34 @@
                 </a>
             </li>
 
+            <li class="nav-item">
+                <a class="nav-link @if(($selected_navigation ?? '') === 'investorSearch') active @endif  " href="{{route('investors.search')}}">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                         class="feather feather-shopping-cart">
+                        <circle cx="9" cy="21" r="1"></circle>
+                        <circle cx="20" cy="21" r="1"></circle>
+                        <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+                    </svg>
+                    <span class="nav-link-text ms-3">ابحث الآن</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link @if(($selected_navigation ?? '') === 'investorFavorite') active @endif  " href="{{route('investors.favorite')}}">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                         class="feather feather-shopping-cart">
+                        <circle cx="9" cy="21" r="1"></circle>
+                        <circle cx="20" cy="21" r="1"></circle>
+                        <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+                    </svg>
+                    <span class="nav-link-text ms-3">المفضلة</span>
+                </a>
+            </li>
+
             <li class="nav-item mt-3 mb-2">
                 <h6 class="ps-4 ms-2 text-uppercase text-muted text-xs opacity-6">{{__('Account pages')}} </h6>
             </li>
