e5c1140947b3647ad848bfa6a127621ffb8315bd
basem <basemgaber01111@gmail.com>
2024-02-29 13:43:21 +0200

    edit file front

--- resources/views/livewire/pioneers/chat/index.blade.php
+++ resources/views/livewire/pioneers/chat/index.blade.php
@@ -1,96 +1,105 @@
-
-<div>
+<div class="box-chat">
 
     <div class="row">
         <div class="col-md-8">
             <div class="preview">
                 @if(!is_null($investor))
-                <div>
-                    <div class="card rounded-0">
-                        <div class="card-header d-flex align-items-center">
-                            <div class="img">
-                                @if($investor->photo && file_exists(url('uploads/' .  $investor->photo)))
-                                    <img src="{{ url('uploads/' .  $investor->photo) }}" alt="logo" width="50" height="50">
-                                @else
-                                    <img src="{{ url('/'. env('DEFAULT_PHOTO')?? "") }}" alt="logo" width="50" height="50">
-                                @endif
-                            </div>
-                            <div class="name ms-3">
-                                {{ $investor->first_name . " " . $investor->last_name }}
-                            </div>
-                        </div>
-                        <div class="card-body" id = "chat_bar">
-
-                            @foreach($messages as $message)
-                            <div class="d-flex w-100 @if($message->sended_by == 'user') justify-content-start  @else justify-content-end  @endif">
-                                <div class="d-flex w-50 p-1 m-1 text-white  mb-2 justify-content-start  @if($message->sended_by == 'user') bg-primary @else bg-info @endif" >
-                                    @if (!is_null($message->file))
-                                        <a href="/download-attachment/{{ $message->file }}" target="_blank">{{ $message->message }}</a>
-                                    @else
-                                        <div class="w-100">
-                            
-                                            <p class="lead mb-0">
-                            
-                                                {{ $message->message }}
-                                            </p>
-                                            <div class="d-flex align-items-center justify-content-between">
-                                                <div></div>
-                                                <div>
-                                                    {{ $message->created_at->format('h:m A') }}
-                                                </div>
-                                            </div>
-                                        </div>
-                                    @endif                                 </div>
-                            </div>
-                            @endforeach
-                        </div>
-                        <div class="card-footer">
-                            <div class="d-flex align-items-center">
-                                <input type="text" id = "message_text" class="form-control" placeholder  = "{{ __('website.chat.placeholder_message') }}" >
-                                <button onclick = 'send({{ $investor->id }})' class = 'btn btn-primary m-0 ms-3' id = 'sendMessage'>
-                                    <i class="fa-solid fa-paper-plane"></i>
-                                </button>
-                            </div>
-                        </div>
+                <div class="card-header bg-primary rounded-0  p-1 d-flex align-items-center" style="height:45.19px;">
+                    <div class="img">
+                        @if($investor->photo && file_exists(url('uploads/' . $investor->photo)))
+                        <img src="{{ url('uploads/' .  $investor->photo) }}" alt="logo" width="50" height="50">
+                        @else
+                        <img src="{{ url('/'. env('DEFAULT_PHOTO')?? "") }}" alt="logo" width="50" height="50">
+                        @endif
                     </div>
+                    <h6 class="mb-0 text-white ms-3">
+                        {{ $investor->first_name . " " . $investor->last_name }}
+                    </h6>
                 </div>
-                @endif           
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="chats">
-                <div class="card rounded-0">
-                    <div class="card-header bg-primary rounded-0  p-1 ">
-                        <h4 class="text-white">{{__('website.messages')}}</h4>
-                    </div>
-                    <div class="card-body">
-                        <div class="search">
-                            <input wire:model = 'search' type="search" class = 'form-control' name = 'search_chat' placeholder="{{ __('website.chat.search') }}">
-                        </div>
-                        @foreach($chats as $chat)
-                        <button class="btn btn-defualt w-100 d-block rounded-0" wire:click = "openChat({{ $chat->investor->id }})">
-                            <div class="box p-2">
-                                <div class="d-flex align-items-center">
-                    
-                                    <div class="img">
-                                        @if($chat->investor->photo && file_exists(url('uploads/' .  $chat->investor->photo)))
-                                            <img src="{{ url('uploads/' .  $chat->investor->photo) }}" alt="logo" width="50" height="50">
-                                        @else
-                                            <img src="{{ url('/'. env('DEFAULT_PHOTO')?? "") }}" alt="logo" width="50" height="50">
-                                        @endif
-                                    </div>
-                                    <div class="name ms-3">
-                                        {{ $chat->investor->first_name . " " . $chat->investor->last_name }}
-                                    </div>
+                <div class="card-body" id="chat_bar">
+
+                    @foreach($messages as $message)
+                    <div
+                        class="d-flex w-100 @if($message->sended_by == 'user') justify-content-start  @else justify-content-end  @endif">
+                        <div class="d-flex p-2 mb-3 justify-content-start @if($message->sended_by == 'user') bg-primary @else bg-light text-dark @endif"
+                            style="max-width:75%; width:fit-content; border-radius: 10px;">
+                            @if (!is_null($message->file))
+                            <a href="/download-attachment/{{ $message->file }}"
+                                target="_blank">{{ $message->message }}</a>
+                            @else
+                            <div class="w-100">
+
+                                <p class="lead mb-0" style="font-size:15px;">
+
+                                    {{ $message->message }}
+                                </p>
+                                <div class="d-flex align-items-center justify-content-between">
+                                    <div></div>
+                                    <span
+                                        style=" @if($message->sended_by == 'user') color:#ffffff99;  @else color: #78787899; @endif">
+                                        {{ $message->created_at->format('h:m A') }}
+                                    </span>
                                 </div>
                             </div>
-                        </button>
-                        @endforeach                    
+                            @endif
+                        </div>
                     </div>
+                    @endforeach
+                    <div class="card-footer">
+                        <div class="d-flex align-items-center">
+                            <div class="inp-file flex-fill">
+                                <input type="text" id="message_text" class="form-control"
+                                    placeholder="{{ __('website.chat.placeholder_message') }}">
+                                <div class="inp">
+                                    <input type="file" name="" id="">
+                                    <i class="fa-solid fa-paperclip"></i>
+                                </div>
+                            </div>
+                            <button onclick='send({{ $investor->id }})' class='btn btn-primary m-0  px-3'
+                                style="    margin-inline-start: 5px !important;" id='sendMessage'>
+                                <i class="fa-solid fa-paper-plane" style="font-size:14px !important;"></i>
+                            </button>
+                        </div>
+                    </div>
+                    @endif
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="chats">
+                        <div class="card-header bg-primary rounded-0  p-1 d-flex align-items-center justify-content-between">
+                        <h6 class="mb-0 text-white">{{__('website.messages')}}</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="search">
+                                <input wire:model='search' type="search" class='form-control' name='search_chat'
+                                    placeholder="{{ __('website.chat.search') }}">
+                            </div>
+                            @foreach($chats as $chat)
+                            <button class="btn btn-defualt w-100 d-block rounded-0"
+                                wire:click="openChat({{ $chat->investor->id }})">
+                                <div class="box p-2">
+                                    <div class="d-flex align-items-center">
+
+                                        <div class="img">
+                                            @if($chat->investor->photo && file_exists(url('uploads/' .
+                                            $chat->investor->photo)))
+                                            <img src="{{ url('uploads/' .  $chat->investor->photo) }}" alt="logo"
+                                                width="50" height="50">
+                                            @else
+                                            <img src="{{ url('/'. env('DEFAULT_PHOTO')?? "") }}" alt="logo" width="50"
+                                                height="50">
+                                            @endif
+                                        </div>
+                                        <div class="name ms-3">
+                                            {{ $chat->investor->first_name . " " . $chat->investor->last_name }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </button>
+                            @endforeach
+                        </div>
                 </div>
             </div>
         </div>
-    </div>    
 
-</div>
-
+    </div>
